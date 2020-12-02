7ffad45302d1fbe9ae8edc0a56bed0d22a7edc5e
Jorge Hernandez <jhernan1337@gmail.com>
2020-12-01 21:53:20 -0700

    Added app.js file along with mysql connection string

---
+++ app.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Password1",
+  database: "employee_db"
+});
+
+    

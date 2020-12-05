178677b51172330d4ad3ad99aaf7e8bc20df5266
Jorge Hernandez <jhernan1337@gmail.com>
2020-12-04 19:05:17 -0700

    Adding ascii art banner

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const mysql = require("mysql");
+const connection = require("./config/connection");
+const inquirer = require("inquirer");
+const figlet = require("figlet");
 
-const connection = mysql.createConnection({
-  host: "localhost",
+function init() {
+  figlet.text(
+    "Employee\n\nManager",
+    {
+      font: "Standard",
+      horizontalLayout: "fitted",
+      verticalLayout: "full",
+      whitespaceBreak: true,
+    },
+    function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+    }
+  );
 
-  // Your port; if not 3306
-  port: 3306,
+  // inquirer.prompt({}).then();
+}
 
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "Password1",
-  database: "employee_db"
-});
-
-    
+init();
